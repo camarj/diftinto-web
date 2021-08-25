e1ea0bd9e577333b7f4a2e99b178db7b6b4a96d1
Raúl Camacho <raulj.camacho@gmail.com>
2021-08-25 13:26:18 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// React Required
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Create Import File
+import './index.scss';
+
+// Common Layout
+// import Layout from "./component/common/App";
+
+import PageScrollTop from './component/PageScrollTop';
+
+// Dark Home Layout 
+import DarkMainDemo from './dark/MainDemo';
+import Contact from "./elements/Contact";
+import error404 from "./elements/error404";
+
+
+
+import { BrowserRouter, Switch, Route  } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+
+class Root extends Component{
+    render(){
+        return(
+            <BrowserRouter basename={'/'}>
+                <PageScrollTop>
+                    <Switch>
+                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={DarkMainDemo}/>
+                        <Route exact path={`${process.env.PUBLIC_URL}/contact`} component={Contact}/>                        
+                        <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
+                        <Route component={error404}/>
+                    </Switch>
+                </PageScrollTop>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
+serviceWorker.register();
